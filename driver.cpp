a48041d60fe558d21dc91ea4c8efe2fb35a8b3ae
Connor McBride <cmm863@mst.edu>
2016-02-03 02:26:17 -0600

    File reading in. Fixed cmake and the .gitignore.

--- driver.cpp
+++ driver.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
+#include <fstream>
 #include "term.h"
 #include "polynomial_fnct.h"
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
+  if(argc < 2) {
+    cout << "No file given." << endl;
+    return 1;
+  }
+
+  string line;
+  ifstream inputFile(argv[1]);
+
+  getline(inputFile, line);
+  cout << line << endl;
   Term<float> t = Term<float>::Term(5.3, 4);
   Term<float> x = Term<float>::Term(28, 5);
   Term<float> y = Term<float>::Term(30, 3);
